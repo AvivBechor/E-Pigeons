02591f17bac1a00a6925086f6672575eaed39285
AvivBechor <avivbechor@gmail.com>
2022-03-03 12:58:28 +0200

    created TextMessage

---
+++ E-Pigeons/E-Pigeons/BasicMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace E_Pigeons
+{
+    public class BasicMessage
+    {
+        private string sync;
+
+        public virtual void SetSync(string _sync)
+        {
+            this.sync = _sync;
+        }
+
+        public virtual string GetSync() => this.sync;
+    }
+}
---
+++ E-Pigeons/E-Pigeons/TextMessage.cs
@@ -0,0 +1,47 @@
+﻿namespace E_Pigeons
+{
+    public class TextMessage:BasicMessage, IDstId, ISrcId, IText
+    {
+        private ushort dstId;
+        private ushort srcId;
+        private string txt;
+
+        public TextMessage(string _sync, ushort _dstId, ushort _srcId, string _txt)
+        {
+            this.SetSync(_sync);
+            this.SetDstId(_dstId);
+            this.SetSrcId(_srcId);
+            this.SetText(_txt);
+        }
+
+        public override void SetSync(string _sync)
+        {
+            if (!int.TryParse(_sync, out int num))
+            {
+                _sync = "0101";
+            }
+            base.SetSync(_sync);
+        }
+
+        public void SetDstId(ushort _dstId)
+        {
+            this.dstId = _dstId;
+        }
+
+        public void SetSrcId(ushort _srcId)
+        {
+            this.srcId = _srcId;
+        }
+
+        public void SetText(string _txt)
+        {
+            this.txt = _txt;
+        }
+
+        public ushort GetDstId() => dstId;
+
+        public ushort GetSrcId() => srcId;
+
+        public string GetText() => txt;
+    }
+}
